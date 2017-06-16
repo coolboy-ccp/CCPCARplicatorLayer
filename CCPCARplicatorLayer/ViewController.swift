01c4f07126334dc5aff9a3c379351cd73d775375
Brian.Chu <chuchengpeng0119@163.com>
2017-06-16 16:37:00 +0800

    新增两种动画
    新增两种动画

--- CCPCARplicatorLayer/ViewController.swift
+++ CCPCARplicatorLayer/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        animation_1()
+        //animation_1()
+//        animation_2()
+        animation_3()
     }
     
     func animation_1() {
@@ -36,9 +38,82 @@ class ViewController: UIViewController {
         lay.instanceCount = 3
         lay.instanceDelay = 0.33
         lay.masksToBounds = true
-        lay.instanceTransform = CATransform3DMakeTranslation(20.0, 0, 0)
+        lay.instanceTransform = CATransform3DMakeTranslation(10.0, 0, 0)
+    }
+    
+    func animation_2() {
+        let lay = CAReplicatorLayer();
+        lay.bounds = CGRect(x: 0, y: 0, width: 200, height: 200)
+        lay.cornerRadius = 10
+        lay.backgroundColor = UIColor(white: 0.0, alpha: 0.75).cgColor
+        lay.position = view.center
+        view.layer.addSublayer(lay)
+        let dot = CALayer()
+        dot.bounds = CGRect(x: 0, y: 0, width: 14, height: 14)
+        dot.position = CGPoint(x:100,y:40)
+        dot.backgroundColor = UIColor(white: 0.8, alpha: 1.0).cgColor
+        dot.backgroundColor = UIColor.white.cgColor
+        dot.borderWidth = 1.0
+        dot.cornerRadius = 2.0
+        lay.addSublayer(dot)
+        let dot_no:Int = 15
+        lay.instanceCount = dot_no
+        let dot_angle = CGFloat(2*M_PI)/CGFloat(dot_no)
+        lay.instanceTransform = CATransform3DMakeRotation(dot_angle, 0, 0, 1.0)
+        let duration:CFTimeInterval = 1.5
+        let scale_animation = CABasicAnimation(keyPath:"transform.scale")
+        scale_animation.fromValue = 1.0
+        scale_animation.toValue = 0.1
+        scale_animation.duration = duration
+        scale_animation.repeatCount = Float.infinity
+        dot.add(scale_animation, forKey: nil)
+        lay.instanceDelay = duration/Double(dot_no)
+        dot.transform = CATransform3DMakeScale(0.01, 0.01, 0.01)
     }
 
+    func animation_3() {
+        let lay = CAReplicatorLayer()
+        lay.bounds = view.bounds
+        lay.backgroundColor = UIColor(white:0.0,alpha:0.75).cgColor
+        lay.position = view.center
+        lay.anchorPoint = CGPoint(x: 0.5, y: 0)
+        view.layer.addSublayer(lay)
+        let dot = CALayer()
+        dot.bounds = CGRect(x: 0, y: 0, width: 10, height: 10)
+        dot.backgroundColor = UIColor(white: 0.8, alpha: 1.0).cgColor
+        dot.borderColor = UIColor.white.cgColor
+        dot.borderWidth = 1.0
+        dot.cornerRadius = 5.0
+        dot.shouldRasterize = true
+        dot.rasterizationScale = UIScreen.main.scale
+        lay.addSublayer(dot)
+        let move = CAKeyframeAnimation(keyPath:"position")
+        move.path = animation_path()
+        move.repeatCount = Float.infinity
+        move.duration = 4.0
+        dot.add(move, forKey: nil)
+        lay.instanceCount = 20
+        lay.instanceDelay = 0.1
+    }
+    
+    func animation_path()->CGPath {
+        let pth = UIBezierPath()
+        pth.move(to: CGPoint(x: 31.5, y: 71.5))
+        pth.addLine(to: CGPoint(x: 31.5, y: 23.5))
+        pth.addCurve(to: CGPoint(x: 58.5, y: 38.5), controlPoint1: CGPoint(x: 31.5, y: 23.5), controlPoint2: CGPoint(x: 53.5, y: 45.5))
+        pth.addLine(to: CGPoint(x: 43.5, y: 48.5))
+        pth.addLine(to: CGPoint(x: 53.5, y: 66.5))
+        pth.addLine(to: CGPoint(x: 62.5, y: 51.5))
+        pth.addLine(to: CGPoint(x: 70.5, y: 66.5))
+        pth.addLine(to: CGPoint(x: 86.5, y: 23.5))
+        pth.addLine(to: CGPoint(x: 86.5, y: 78.5))
+        pth.addLine(to: CGPoint(x: 31.5, y: 78.5))
+        pth.addLine(to: CGPoint(x: 31.5, y: 71.5))
+        pth.close()
+        var t = CGAffineTransform(scaleX: 3.0,y: 3.0)
+        return pth.cgPath.copy(using: &t)!
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
